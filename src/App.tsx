6ce65694bca8c49a048c11d4ea2ea97f56dd3985
leilei405 <1601178425@qq.com>
2024-08-14 23:53:39 +0800

    feat: Button 组件编码  边框黑色未解决

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import React from "react";
+import Button from "./components/Button";
 function App() {
-  return <div className="App">测试提交</div>;
+  return (
+    <div className="App">
+      <Button style={{ border: "none" }} type="primary" size="large">
+        button
+      </Button>
+    </div>
+  );
 }
 
 export default App;
