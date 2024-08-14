8fd94f5bd0a8efb1777eb0aaf69221508fb63923
leilei405 <1601178425@qq.com>
2024-08-15 00:40:06 +0800

    feat: 修改Primary 按钮点击的样式

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,29 @@ import React from "react";
 import Button from "./components/Button";
 function App() {
   return (
-    <div className="App">
-      <Button style={{ border: "none" }} type="primary" size="large">
-        button
-      </Button>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
+      <div style={{ width: 500 }}>
+        <Button style={{ margin: 5 }} type="primary" size="large">
+          button
+        </Button>
+        <Button style={{ margin: 5 }} type="dark" size="large">
+          button
+        </Button>
+        <Button style={{ margin: 5 }} type="link" size="large">
+          button
+        </Button>
+        <Button style={{ margin: 5 }} type="success" size="large">
+          button
+        </Button>
+      </div>
     </div>
   );
 }
