92121b9a6d70047bad4df526de28a6c6d0cd9b1b
leilei405 <1601178425@qq.com>
2024-08-19 00:08:20 +0800

    update: 修改Menu组件样式

--- src/components/Menu/use.tsx
+++ src/components/Menu/use.tsx
@@ -6,14 +6,29 @@ const MenuCom = () => {
     console.log("click", idx);
   };
   return (
-    <Menu defaultIndex="0" onSelect={(idx) => handleClick(idx)}>
-      <MenuItem activeKey={1}>menu1</MenuItem>
-      <MenuItem activeKey={2} disabled>
-        menu2
-      </MenuItem>
-      <MenuItem activeKey={3}>menu3</MenuItem>
-      <MenuItem activeKey={4}>menu4</MenuItem>
-    </Menu>
+    <div style={{ display: "flex", justifyContent: "space-around" }}>
+      <Menu
+        mode="vertical"
+        defaultIndex="0"
+        onSelect={(idx) => handleClick(idx)}
+      >
+        <MenuItem activeKey={1}>menu1</MenuItem>
+        <MenuItem activeKey={2} disabled>
+          menu2
+        </MenuItem>
+        <MenuItem activeKey={3}>menu3</MenuItem>
+        <MenuItem activeKey={4}>menu4</MenuItem>
+      </Menu>
+
+      <Menu defaultIndex="0" onSelect={(idx) => handleClick(idx)}>
+        <MenuItem activeKey={1}>menu1</MenuItem>
+        <MenuItem activeKey={2} disabled>
+          menu2
+        </MenuItem>
+        <MenuItem activeKey={3}>menu3</MenuItem>
+        <MenuItem activeKey={4}>menu4</MenuItem>
+      </Menu>
+    </div>
   );
 };
 
