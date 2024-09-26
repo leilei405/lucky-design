8f0b1e60a19f81c53ea207dbc4446d98ff59292f
leilei405 <1601178425@qq.com>
2024-09-26 23:57:05 +0800

    docs: 📝更新样式文档

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -17,93 +17,84 @@ const meta = {
 } satisfies Meta<typeof Button>;
 
 export default meta;
-type Story = StoryObj<typeof meta>;
 
-type ArgsProps = ButtonProps;
+type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: { type: "default", disabled: false, size: "small" },
-  render: (args: ArgsProps) => (
+  render: (args: ButtonProps) => (
     <Button {...args} type="default">
       Default
     </Button>
   ),
-  storyName: "默认按钮",
 };
 
 export const Primary: Story = {
   args: { type: "primary", disabled: false, size: "small" },
-  render: (args: ArgsProps) => (
+  render: (args: ButtonProps) => (
     <Button {...args} type="primary">
       Primary
     </Button>
   ),
-  storyName: "主要按钮",
 };
 
 export const Success: Story = {
   args: { type: "success", disabled: false, size: "small" },
-  render: (args: ArgsProps) => (
+  render: (args: ButtonProps) => (
     <Button {...args} type="success">
       Success
     </Button>
   ),
-  storyName: "成功按钮",
 };
 
 export const Danger: Story = {
   args: { type: "danger", disabled: false, size: "small" },
-  render: (args: ArgsProps) => (
+  render: (args: ButtonProps) => (
     <Button {...args} type="danger">
       Danger
     </Button>
   ),
-  storyName: "危险按钮",
 };
 
 export const Link: Story = {
   args: { type: "link", disabled: false, size: "small" },
-  render: (args: ArgsProps) => (
+  render: (args: ButtonProps) => (
     <Button {...args} type="link">
       Link
     </Button>
   ),
-  storyName: "链接按钮",
 };
 
 export const Secondary: Story = {
   args: { type: "secondary", disabled: false, size: "small" },
-  render: (args: ArgsProps) => (
+  render: (args: ButtonProps) => (
     <Button {...args} type="secondary">
       Secondary
     </Button>
   ),
-  storyName: "次要按钮",
 };
 
 export const Info: Story = {
   args: { type: "info", disabled: false, size: "small" },
-  render: (args: ArgsProps) => (
+  render: (args: ButtonProps) => (
     <Button {...args} type="info">
       Info
     </Button>
   ),
-  storyName: "信息按钮",
 };
 
 export const Warning: Story = {
   args: { type: "warning", disabled: false, size: "small" },
-  render: (args: ArgsProps) => (
+  render: (args: ButtonProps) => (
     <Button {...args} type="warning">
       Warning
     </Button>
   ),
-  storyName: "警告按钮",
 };
 
 export const Light: Story = {
   args: { type: "light", disabled: false, size: "small" },
-  render: (args: ArgsProps) => (
+  render: (args: ButtonProps) => (
     <Button {...args} type="light">
       Light
     </Button>
@@ -112,21 +103,20 @@ export const Light: Story = {
 
 export const Dark: Story = {
   args: { type: "dark", disabled: false, size: "small" },
-  render: (args: ArgsProps) => (
+  render: (args: ButtonProps) => (
     <Button {...args} type="dark">
       Dark
     </Button>
   ),
-  storyName: "深色按钮",
 };
 
-// Default.storyName = "默认按钮";
-// Primary.storyName = "主要按钮";
-// Success.storyName = "成功按钮";
-// Danger.storyName = "危险按钮";
-// Link.storyName = "跳转按钮";
-// Secondary.storyName = "次要按钮";
-// Info.storyName = "信息按钮";
-// Warning.storyName = "告警按钮";
-// Light.storyName = "亮色按钮";
-// Dark.storyName = "暗色按钮";
+Default.storyName = "默认按钮";
+Primary.storyName = "主要按钮";
+Success.storyName = "成功按钮";
+Danger.storyName = "危险按钮";
+Link.storyName = "跳转按钮";
+Secondary.storyName = "次要按钮";
+Info.storyName = "信息按钮";
+Warning.storyName = "告警按钮";
+Light.storyName = "亮色按钮";
+Dark.storyName = "暗色按钮";
